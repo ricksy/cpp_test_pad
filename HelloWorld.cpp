65051c93a78c32d644a8397bede75ea0f0b23a2d
ricksy <786869+ricksy@users.noreply.github.com>
2021-11-28 21:59:56 +0100

    added git commit hash to binary

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+inline void LogGitCommitHash() {
+	#ifndef GIT_COMMIT_HASH
+	#define GIT_COMMIT_HASH "0000000" // 0000000 means uninitialized
+	#endif
+	    std::cout << "GIT_COMMIT_HASH[" << GIT_COMMIT_HASH << "]\n"; // 4f34ee8
+}
 int main (int argc, char** argv)
 {
-std::cout<<"Hello World!\n";
-return 0;
+
+	LogGitCommitHash();
+
+	return 0;
 }
